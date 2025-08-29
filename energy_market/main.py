d10149f4d85131e204728bc98fc6344f7bca7934
GodwinOkwuibe <chidi@DESKTOP-8871O20.localdomain>
2025-08-29 16:13:09 +0200

    Updated license

--- energy_market/main.py
+++ energy_market/main.py
@@ -14,7 +14,7 @@ data = pd.read_csv(os.path.join(curr_dir, "data/"+str(marktSlots)+'.csv'), delim
 
 Nslot = 94
 startSlot = "2014-12-01T00:00"
-grid_fee = 4 #grid fee in cent/kWh
+grid_fee = 2 #grid fee in cent/kWh
 simu = sm(data,startSlot, Nslot, "p2p", grid_fee) #mechanism can be "uniform", "p2p", "hhc"
 #simu = sm(pub_ins)
 prev_slot, index = simu.simulate()
